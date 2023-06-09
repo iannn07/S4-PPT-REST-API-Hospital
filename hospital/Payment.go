238d1fed01bb2a1fbae3db35db6d6cbee79fac5a
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-09 21:03:38 +0700

    FIX BUG

--- hospital/Payment.go
+++ hospital/Payment.go
@@ -1,7 +1,23 @@
 package hospital
 
 type Payment struct {
-	PayID     int    `json:"PayID"  gorm:"column:PayID;primaryKey"`
-	PatientID int    `json:"PatientID"`
-	PayTotal  string `json:"PayTotal"`
+	PayID     int     `gorm:"column:pay_id;int;primaryKey;autoIncrement"	json:"pay_id"`
+	PatientID int     `gorm:"column:patient_id;int"							json:"patient_id"`
+	PayTotal  string  `gorm:"column:pay_total;varchar(255)"					json:"pay_total"`
+	Patient   Patient `gorm:"foreignKey:PatientID"`
+}
+
+type PaymentInput struct {
+	PatientID int    `json:"patient_id"`
+	PayTotal  string `json:"pay_total"`
+}
+
+type PaymentResponse struct {
+	PayID     int    			`json:"pay_id"`
+	PatientID int    			`json:"patient_id"`
+	PayTotal  string 			`json:"pay_total"`
+}
+
+type PaymentListResponse struct {
+	Payments []PaymentResponse `json:"payments"`
 }
