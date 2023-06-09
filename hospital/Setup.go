197ab140dedfbdc8267082dad93118d377564b3d
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-09 21:04:01 +0700

    FIX BUG

--- hospital/Setup.go
+++ hospital/Setup.go
@@ -9,8 +9,16 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	db, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/finpro_ppt"), &gorm.Config{})
+	
 	if err != nil {
 		panic("Failed to connect!")
 	}
+
+	db.AutoMigrate(&Doctor{})
+	db.AutoMigrate(&Patient{})
+	db.AutoMigrate(&Room{})
+	db.AutoMigrate(&Diagnose{})
+	db.AutoMigrate(&Payment{})
+
 	DB = db
 }
