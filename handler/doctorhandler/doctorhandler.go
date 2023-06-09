b6b03ecbf6cbff8b65c097938458a2ee4018d5e1
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-09 21:04:42 +0700

    FIX BUG

--- handler/doctorhandler/doctorhandler.go
+++ handler/doctorhandler/doctorhandler.go
@@ -3,78 +3,111 @@ package doctorhandler
 import (
 	"fmt"
 	"net/http"
-
 	"HospitalFinpro/hospital"
-
 	"github.com/gin-gonic/gin"
 )
 
 func SelectAll(c *gin.Context) {
 	var doctors []hospital.Doctor
 	hospital.DB.Find(&doctors)
-	c.JSON(http.StatusOK, gin.H{"data": doctors})
+
+	var doctorResponses []hospital.DoctorResponse
+	for _, doctor := range doctors {
+		doctorResponse := hospital.DoctorResponse{
+			DoctorID:      doctor.DoctorID,
+			DoctorName:    doctor.DoctorName,
+			DoctorLicense: doctor.DoctorLicense,
+		}
+		doctorResponses = append(doctorResponses, doctorResponse)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": doctorResponses})
 }
 
 func Create(c *gin.Context) {
-	var input hospital.Doctor
-	if err := c.ShouldBindJSON(&input); err != nil {
-		fmt.Println("Error binding JSON:", err.Error())
+	var doctorInput hospital.DoctorInput
+	if err := c.ShouldBindJSON(&doctorInput); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println("Received JSON data:", input)
+
 	doctor := hospital.Doctor{
-		DoctorName:    input.DoctorName,
-		DoctorLicense: input.DoctorLicense,
+		DoctorName:    doctorInput.DoctorName,
+		DoctorLicense: doctorInput.DoctorLicense,
 	}
-	fmt.Println("Creating doctor:", doctor)
-	hospital.DB.Create(&doctor)
-	fmt.Println("Doctor created:", doctor)
-	c.JSON(http.StatusOK, gin.H{"data": doctor})
+
+	if err := hospital.DB.Create(&doctor).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create doctor"})
+		return
+	}
+
+	doctorResponse := hospital.DoctorResponse{
+		DoctorID:      doctor.DoctorID,
+		DoctorName:    doctor.DoctorName,
+		DoctorLicense: doctor.DoctorLicense,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": doctorResponse})
 }
-
-// func Create(c *gin.Context) {
-// 	var input hospital.Doctor
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	doctor := hospital.Doctor{DoctorName: input.DoctorName, DoctorLicense: input.DoctorLicense}
-// 	hospital.DB.Create(&doctor)
-
-// 	c.JSON(http.StatusOK, gin.H{"data": doctor})
-// }
 
 func Read(c *gin.Context) {
 	var doctor hospital.Doctor
-	if err := hospital.DB.Where("DoctorID = ?", c.Param("id")).First(&doctor).Error; err != nil {
+	if err := hospital.DB.Where("doctor_id = ?", c.Param("id")).First(&doctor).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "DATA TIDAK DITEMUKAN!"})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": doctor})
+
+	doctorResponse := hospital.DoctorResponse{
+		DoctorID:      doctor.DoctorID,
+		DoctorName:    doctor.DoctorName,
+		DoctorLicense: doctor.DoctorLicense,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": doctorResponse})
 }
 
 func Update(c *gin.Context) {
 	var doctor hospital.Doctor
-	if err := hospital.DB.Where("DoctorID = ?", c.Param("id")).First(&doctor).Error; err != nil {
+	fmt.Println(c.Param("id"))
+	if err := hospital.DB.Where("doctor_id = ?", c.Param("id")).First(&doctor).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "DATA TIDAK DITEMUKAN!"})
 		return
 	}
-	var input hospital.Doctor
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "NO DATA!"})
+
+	var doctorInput hospital.DoctorInput
+	if err := c.ShouldBindJSON(&doctorInput); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	hospital.DB.Model(&doctor).Updates(input)
-	c.JSON(http.StatusOK, gin.H{"data": doctor})
+
+	doctor.DoctorName = doctorInput.DoctorName
+	doctor.DoctorLicense = doctorInput.DoctorLicense
+
+	if err := hospital.DB.Save(&doctor).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update doctor"})
+		return
+	}
+
+	doctorResponse := hospital.DoctorResponse{
+		DoctorID:      doctor.DoctorID,
+		DoctorName:    doctor.DoctorName,
+		DoctorLicense: doctor.DoctorLicense,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": doctorResponse})
 }
 
 func Delete(c *gin.Context) {
 	var doctor hospital.Doctor
-	if err := hospital.DB.Where("DoctorID = ?", c.Param("id")).First(&doctor).Error; err != nil {
+	if err := hospital.DB.Where("doctor_id = ?", c.Param("id")).First(&doctor).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "DATA TIDAK DITEMUKAN!"})
 		return
 	}
-	hospital.DB.Delete(&doctor)
+
+	if err := hospital.DB.Delete(&doctor).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete doctor"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"data": true})
 }
