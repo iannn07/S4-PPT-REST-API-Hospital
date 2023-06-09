378fb010655953624b8efa914149d7b2d2cd451c
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-09 21:03:10 +0700

    FIX BUG

--- hospital/Doctor.go
+++ hospital/Doctor.go
@@ -1,7 +1,22 @@
 package hospital
 
 type Doctor struct {
-	DoctorID      int    `json:"DoctorID" gorm:"column:DoctorID;primaryKey"`
+	DoctorID        int    `gorm:"column:doctor_id;int;primaryKey;autoIncrement"	json:"doctor_id"`
+	DoctorName   	string `gorm:"column:doctor_name;varchar(255)"					json:"doctor_name"`
+	DoctorLicense 	string `gorm:"column:doctor_license;varchar(255)"				json:"doctor_license"`
+}
+
+type DoctorInput struct {
 	DoctorName    string `json:"doctor_name"`
 	DoctorLicense string `json:"doctor_license"`
+}
+
+type DoctorResponse struct {
+	DoctorID        int    `json:"doctor_id"`
+	DoctorName   	string `json:"doctor_name"`
+	DoctorLicense 	string `json:"doctor_license"`
+}
+
+type DoctorListResponse struct {
+	Doctors []DoctorResponse `json:"doctors"`
 }
